ebe640749f6910b675c219b30a3d25603a7a194e
DefinitelyNotAbhishek <110118289+DefinitelyNotAbhishek@users.noreply.github.com>
2024-06-09 13:03:40 +0530

    Learnt basic express

---
+++ 2.1 ExpressBaic.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const cricketers = [
+  {
+    fullname: "Rohit Sharma",
+    role: "Opener",
+    trophies: [
+      {
+        individual: false,
+      },
+    ],
+  },
+];
+
+const app = express();
+
+app.get("/", function (req, res) {
+  const rohitTrophies = cricketers[0].trophies;
+  const numberOfTrophy = rohitTrophies.length;
+  let nonindividual = 0;
+  for (i = 0; i < numberOfTrophy; i++) {
+    if (rohitTrophies[i].individual) {
+      nonindividual = nonindividual + 1;
+    }
+  }
+  let individual = numberOfTrophy - nonindividual;
+
+  res.json({ numberOfTrophy, nonindividual, individual });
+});
+
+app.listen(3001);
