a593d4442bb9e83c934aff90de94a96d95fd65f7
DefinitelyNotAbhishek <110118289+DefinitelyNotAbhishek@users.noreply.github.com>
2024-06-11 20:54:20 +0530

    backend using middlewares

---
+++ 3.1-aftermiddlewares.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+function middlewareAuthentication(req, res, next) {
+  const username = req.headers.username;
+  const password = req.headers.password;
+  if (username != "Abhishek" || password != "Ab3384") {
+    res.status(400).json({
+      msg: "You are not a authorized user ",
+    });
+  } else {
+    next();
+  }
+}
+
+function middlewareInput(req, res, next) {
+  const birthYear = req.query.birthYear;
+  if (birthYear != 2001 && birthYear != 2000) {
+    res.status(400).json({
+      msg: " Naah nigga you are not 90s kid ",
+    });
+  } else {
+    next();
+  }
+}
+
+app.use(middlewareAuthentication);
+app.use(middlewareInput);
+
+app.get("/mid", (req, res) => {
+  res.send("Hii You are here");
+});
+
+app.listen(3001);
